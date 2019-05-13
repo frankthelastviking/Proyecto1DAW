ed0801566a62ed0146deb3720a29c8ae115d18ee
ElMonjeDeVerdad <guselcalvo@gmail.com>
2019-05-13 10:20:27 +0200

    Añadidas las clases siguientes: AlumnosController,MainController,TestConexion y el archivo configuracion.ini

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,26 +1,76 @@
 package application;
-	
+
+import java.io.IOException;
+
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.AnchorPane;
 
 
 public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static void main(String[] args) {
-		launch(args);
-	}
+
+    private static Stage stagePrincipal;
+    private AnchorPane rootPane;
+
+    @Override
+    public void start(Stage stagePrincipal) throws Exception {
+        Main.stagePrincipal = stagePrincipal;
+        mostrarVentanaPrincipal();
+
+    }
+
+    /* En un proyecto JavaFX el main llama al launch que a su vez llama a start */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    /*
+     * cargamos la ventana principal
+     */
+    public void mostrarVentanaPrincipal() {
+        try {
+            FXMLLoader loader = new FXMLLoader(Main.class.getResource("../application/Main.fxml"));
+            rootPane=(AnchorPane) loader.load();
+            Scene scene = new Scene(rootPane);
+            stagePrincipal.setTitle("Ventana Principal");
+            stagePrincipal.setScene(scene);
+            /*
+             * Añadidos las llamadas del main al Controlador y del controlador al main ***/
+            MainController controller = loader.getController();
+            controller.setProgramaPrincipal(this);
+
+            stagePrincipal.show();
+        } catch (IOException e) {
+            //tratar la excepción.
+        }
+   }
+
+
+    /* Este método es llamado cuando se presiona el botón de la ventana principal
+     * Lo llama el controlador de la vista principal
+     */
+    public void VentanaAlumnos() {
+        try {
+            FXMLLoader loader = new FXMLLoader(Main.class.getResource("../vista/VentanaDos.fxml"));
+            AnchorPane ventanaDos = (AnchorPane) loader.load();
+            /* Creamos la segunda ventana como otro stage */
+            Stage ventana = new Stage();
+            ventana.setTitle("Alumnos");
+            /* Le decimos a la ventana quién es la ventana original */
+            ventana.initOwner(stagePrincipal);
+            Scene scene = new Scene(ventanaDos);
+            ventana.setScene(scene);
+
+            AlumnosController controller2 = loader.getController();
+            controller2.setStagePrincipal(ventana);
+
+            ventana.show();
+
+        } catch (Exception e) {
+            //tratar la excepción
+        }
+    }
 }
